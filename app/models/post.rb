50816d3dbeb0ad93eef11e61128861beb54cd60c
Serje <Serje.havandjian@gmail.com>
2022-06-28 14:04:50 -0700

    commit tree e19fb3449e3529885fe7a9264836e2e611b7fdb4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :clickbait
+    
+    def clickbait
+        
+        unless title && title.include?("Won't Believe")
+            errors.add(:title, "no")
+        end
+    end
+
 end
+
+
+# && title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+#             errors.add(:title, "no")
+#         end
